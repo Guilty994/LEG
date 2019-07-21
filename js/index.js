ef8a49efda720736cc32950f66692f7027eb1bbd
Daniele Lupo <d.lupo1@studenti.unisa.it>
2019-07-21 18:03:29 +0200

    Pulizia codice
    Rimossi commenti e log superflui

--- js/index.js
+++ js/index.js
@@ -2,9 +2,10 @@ var global_name;
 var global_appId;
 
 
+// Per la ricerca premendo invio
 function handle(e) {
     if (e.keyCode === 13) {
-        e.preventDefault(); // Ensure it is only this code that rusn
+        e.preventDefault();
         cerca();
     }
 }
@@ -13,20 +14,15 @@ function cerca() {
     if ($("#nomeGioco").val().length == 0) return;
     $("#ricercheRecenti").hide(500);
     $.ajax({
-        url: "./controller.php?game=" + $("#nomeGioco").val() + "&source=steam", // &source=steam aggiunto per testing poi si vede come fare
+        url: "./controller.php?game=" + $("#nomeGioco").val() + "&source=steam",
         statusCode: {
             200: function (response) {
                 let dati = JSON.parse(response);
-                //let dati = JSON.parse(response.split("JSON")[1]);
-                console.log(dati);
 
                 global_appId = dati.appId;
-
-                // Dati presi da Steam
-
-                // Nome gioco
-                $("#labelNomeGioco").text(dati.gameName);
                 global_name = dati.gameName;
+
+                $("#cardPrezzi").html("");
 
                 getFromYoutube(global_name);
                 getFromSteamCharts(global_appId);
@@ -58,6 +54,9 @@ function cerca() {
 }
 
 function stampaDatiSteam(dati) {
+    // Nome gioco
+    $("#labelNomeGioco").text(dati.gameName);
+
     // Immagine di copertina
     $("#copertina").attr("src", dati.gameImage);
 
@@ -91,30 +90,11 @@ function stampaDatiSteam(dati) {
         $("#cardTrend").hide(0);
 
     // ScreenShots
-
-    /*<div id="carouselScreenshots" class="carousel slide" data-ride="carousel">
-                  <!-- Indicators -->
-                  <ol class="carousel-indicators" id="carouselIndicatorsScreenshots"></ol>
-
-                  <!-- Wrapper for slides -->
-                  <div class="carousel-inner" id="carouselInnerScreenshots" style="text-align: center"></div>
-
-                  <!-- Left and right controls -->
-                   <a class="left carousel-control" href="#carouselScreenshots" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="right carousel-control" href="#carouselScreenshots" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </div>*/
     str = '<div id="carouselScreenshots" class="carousel slide" data-ride="carousel">';
     str += '<ol class="carousel-indicators" id="carouselIndicatorsScreenshots">';
     let listaImmagini = "";
     for (let index = 0; index < dati.gameScreenshot.length; index++) {
         str += '<li data-target="#carouselScreenshots" data-slide-to="' + index + '"' + (index == 0 ? 'class="active"' : '') + '></li>';
-        //listaImmagini += '<div class="item ' + (index == 0 ? 'active' : '') + '"><center><img width="100%" src="' + dati.gameScreenshot[index] + '"></center></div>';
     }
     str+= '</ol><div class="carousel-inner" id="carouselInnerScreenshots" style="text-align: center">';
     for (let index = 0; index < dati.gameScreenshot.length; index++) {
@@ -124,10 +104,6 @@ function stampaDatiSteam(dati) {
     str += '<a class="left carousel-control" href="#carouselScreenshots" data-slide="prev">';
     str += '<span class="glyphicon glyphicon-chevron-left"></span><span class="sr-only">Previous</span></a>';
     str += '<a class="right carousel-control" href="#carouselScreenshots" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span><span class="sr-only">Next</span></a>';
-    /*$("#carouselIndicatorsScreenshots").html("");
-    $("#carouselIndicatorsScreenshots").html(str);
-    $("#carouselInnerScreenshots").html("");
-    $("#carouselInnerScreenshots").html(listaImmagini);*/
     $("#rowSlideshow").html(str);
     // Fine dati Steam
     $("#risultatiRicerca").show(500);
@@ -138,7 +114,6 @@ function getFromSteamCharts(steam_appid) {
         url: "./controller.php?steam_appid=" + steam_appid + "&source=steamcharts",
         statusCode: {
             200: function (response) {
-                console.log(response);
                 response = JSON.parse(response);
                 $("#steamCharts").text("AVG 30 days players: " + response.avg + ". The peak is:" + response.peak);
             },
@@ -160,7 +135,6 @@ function getFromTwitch(steam_name) {
         url: "./controller.php?game=" + steam_name + "&source=twitch",
         statusCode: {
             200: function (response) {
-                console.log(response);
                 response = JSON.parse(response);
                 $("#cardTrend").show(0);
                 $("#twitchViewers").text('Twitch viewers: ' + response.twitchViewers);
@@ -183,9 +157,7 @@ function getFromYoutube(steam_name) {
         url: "./controller.php?game=" + steam_name + "&source=youtube",
         statusCode: {
             200: function (response) {
-                console.log(response);
                 response = JSON.parse(response);
-                console.log();
                 $("#gameplayYoutube0").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + response["videoGameplay"][0].split("?v=")[1] + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
                 $("#gameplayYoutube1").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + response["videoGameplay"][1].split("?v=")[1] + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
             },
@@ -207,7 +179,6 @@ function getFromGreenman(steam_name) {
         url: "./controller.php?game=" + steam_name + "&source=greenman",
         statusCode: {
             200: function (response) {
-                console.log(response);
                 response = JSON.parse(response);
                 str = $("#cardPrezzi").html();
                 str += '<div class="col-md-4" style="text-align:center"><a href="' + response.greenManGameURL + '"><img style="width:100%;padding-top:25%" src="./logoGreenManGaming.png"></img><br><p class="text-muted">' + response.greenManPrice.replace('EUR','') + '</p></a></div>';
@@ -231,7 +202,6 @@ function getFromG2A(steam_name) {
         url: "./controller.php?game=" + steam_name + "&source=g2a",
         statusCode: {
             200: function (response) {
-                console.log(response);
                 response = JSON.parse(response);
                 str = $("#cardPrezzi").html();
                 str += '<div class="col-md-4" style="text-align:center"><a href="' + response.G2AGameURL + '"><img style="width:100%;padding-top:30%" src="./logoG2A.jpg"></img><br><p class="text-muted">' + response.G2AGamePrice.replace('EUR','') + '</p></a></div>';
@@ -255,12 +225,10 @@ function getFromKinguin(steam_name) {
         url: "./controller.php?game=" + steam_name + "&source=kinguin",
         statusCode: {
             200: function (response) {
-                console.log(response);
                 response = JSON.parse(response);
                 str = $("#cardPrezzi").html();
                 str += '<div class="col-md-4" style="text-align:center"><a href="' + response.kinguinGameURL + '"><img style="width:100%" src="./logoKinguin.jpg"></img><br><p class="text-muted">' + response.kinguinGamePrice + '</p></a></div>';
                 $("#cardPrezzi").html(str);
-                console.info(str);
             },
             400: function () {
                 alert("Parametri errati per Greenman");
