942d0d3fd1537e83707abf0ed1e4e705848f73e8
Guilty994 <mondaynight1@live.it>
2019-08-02 17:15:11 +0200

    searchbytags v1

--- debugindex.php
+++ debugindex.php
@@ -1,6 +1,15 @@
 <?php
     include "simple_html_dom.php";
+
     $toReturn = array();
-    
-    include "wrappers/wrapper_steam_searchbytag.php"
+
+    $_GET['tags']="19,21,9";
+    if(!isset($_GET["tags"])){
+        header("Tags not set", true, 400);
+        exit;
+    }
+    $tags_tosearch = $_GET['tags'];
+    $tags_tosearch = str_replace (",", "%2C", $tags_tosearch);
+
+    include "wrappers/wrapper_steam_searchbytag.php";
 ?>
--- wrappers/wrapper_steam_searchbytag.php
+++ wrappers/wrapper_steam_searchbytag.php
@@ -1,11 +1,13 @@
 <?php
-/*
-    I tags sono contenuti nel file tags.json
+    /*
+        I tags sono contenuti nel file tags.json
+    */
+    //encoding tags 
+    // controllo tags delegato a controller
 
-*/
-$curl = curl_init("https://store.steampowered.com/search/?ignore_preferences=1&category1=998");
+    $curl = curl_init("https://store.steampowered.com/search/?ignore_preferences=1&tags=" . $tags_tosearch . "&category1=998");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-    
+
     $response = curl_exec($curl);
     if(curl_errno($curl)){
         header("wrapper_steam, Scraper error: " . curl_error($curl), true, 500);
@@ -15,5 +17,11 @@ $curl = curl_init("https://store.steampowered.com/search/?ignore_preferences=1&c
 
     $html = new simple_html_dom();
     $html -> load($response);
+
+    foreach($html->find('span') as $span){
+        if($span->class == 'title'){
+            echo $span . '<br>';
+        }
+    }
 
 ?>
