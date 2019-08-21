1d04402b47a667988f1ca21b3681e82fd27331f3
Daniele Lupo <d.lupo1@studenti.unisa.it>
2019-08-21 23:48:57 +0200

    Risolto (forse) il problema dei nomi

--- js/index.js
+++ js/index.js
@@ -105,13 +105,12 @@ function caricaRecenti(recenti) {
         let gioco = recenti[r];
         str += '<div class="col-md-4" onclick="recupera(' + r + ')" style="padding:1%">';
         str += '<img class="img-responsive" src="' + gioco.datiSteam.gameImage + '" alt="Immagine di copertina">';
-        if(gioco.datiSteam.gameName.length > 48){
+        str += '<p class="text-center"><b>' + gioco.datiSteam.gameName + '</b></p>';
+        /*if(gioco.datiSteam.gameName.length > 24){
             str += '<h6 class="text-center">' + gioco.datiSteam.gameName + '</h6>';
-        }else if(gioco.datiSteam.gameName.length > 24){
-            str += '<h5 class="text-center">' + gioco.datiSteam.gameName + '</h5>';
         }else{
             str += '<h4 class="text-center">' + gioco.datiSteam.gameName + '</h4>';
-        }
+        }*/
         str += '</div>';
     }
     str += '</div>';
